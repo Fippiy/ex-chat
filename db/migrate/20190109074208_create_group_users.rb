88df470996cfb197d38b9b1036018cbf90a97e6d
Fippiy <profier4@gmail.com>
2019-01-09 16:47:28 +0900

    Edit migration file

--- db/migrate/20190109074208_create_group_users.rb
+++ db/migrate/20190109074208_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :group_users do |t|
-      t.refercences :group, foreigh_key: true
-      t.refercences :user, foreigh_key: true
+      t.references :group, foreigh_key: true
+      t.references :user, foreigh_key: true
       t.timestamps
     end
   end
