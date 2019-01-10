302862ac45cbea9f76f84a4f5d18be194b410ba6
Fippiy <profier4@gmail.com>
2019-01-10 20:03:45 +0900

    edit group edit system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  # root 'messages#index'
+  root 'groups#index'
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index]
   end
